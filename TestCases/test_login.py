7e9e76ce03c8270a41f62709a061501cc85ce79f
niujinliang <2415793@qq.com>
2022-08-23 23:48:52 +0800

    5

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -3,6 +3,8 @@ from selenium import webdriver
 import ddt
 from PageObjects.login_page import LoginPage
 from PageObjects.home_page import HomePage
+from TestDates import login_datas as lds
+from TestDates import Global_Datas as GD
 
 
 @ddt.ddt
@@ -11,7 +13,7 @@ class TestLogin(unittest.TestCase):
     def setUp(self) -> None:
         # 访问登录页面
         self.driver = webdriver.Chrome()
-        self.driver.get("https://www.baidu.com")
+        self.driver.get(GD.base_url)
         self.driver.maximize_window()
 
     def tearDown(self) -> None:
@@ -19,17 +21,11 @@ class TestLogin(unittest.TestCase):
 
     def test_login_success(self):
         # 步骤 1、登录页面-登录操作  用户名密码
-        LoginPage(self.driver).login("123423", "python")
+        LoginPage(self.driver).login(*lds.success)
         # 断言 首页- 获取元素是否存在
         self.assertTrue(HomePage(self.drvier).get_element_exists())
 
-    cases = [
-        {"user": "", "passwd": "python", "check": "请输入手机号"},
-        {"user": "1234234343", "passwd": "", "check": "请输入密码"},
-        {"user": "1234234343", "passwd": "", "check": "请输入正确手机号"}
-    ]
-
-    @ddt.data(*cases)
+    @ddt.data(*lds.cases)
     def test_login_failed_wrong_format(self, case):
         # 步骤 1、登陆页面 登录操作 用户名密码期望数据
         lp = LoginPage(self.driver)
